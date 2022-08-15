1b5d817bd5fe87f86a3f4b3a40124e8805c2bcc8
Iani Gayo <i_jirahmae@me.com>
2022-08-15 14:16:29 +0100

    training script to run models

---
+++ training_script.py
@@ -0,0 +1,25 @@
+import numpy as np
+import torch
+from utils import * 
+from unet_network import * 
+from torch.utils.data import Dataset, DataLoader, RandomSampler 
+
+if __name__ == '__main__':
+
+    # TODO - fix dataloader to take in n num of images (30) at a time rather than the whole dataset
+    FOLDER_NAME = '/Users/ianijirahmae/Documents/Data_folder'
+    train_loader = DataLoader(Image_dataloader(FOLDER_NAME, mode = 'train'), batch_size = 4, shuffle = True)
+    val_loader = DataLoader(Image_dataloader(FOLDER_NAME, mode = 'train'), batch_size = 1)
+
+    # Initialising the model 
+    model = UNet_3D(1, 1)
+
+    # Initialising the training procedure 
+    experiment_name = 'baseline'
+    train_loss, val_loss, train_iou, val_iou = train(model, train_loader, val_loader, \
+        num_epochs = 10, use_cuda = False, save_folder = experiment_name)
+
+    print('Chicken')
+    
+
+
